ef0b9b1219a411280bfbb10f5996c12e9887842e
Priyesh Anand <Priyesh.Anand@godigit.com>
2024-07-25 06:35:34 +0530

    get_user_by_full_name functionality

--- user-data.js
+++ user-data.js
@@ -17,3 +17,24 @@ function add_user(){
     employee_details.push(user_obj);
 }
 
+
+/*    GET USER BY FULL NAME     */
+
+function get_user_by_full_name()
+{
+const f_name = prompt("Enter the full name of the user :")
+const result = employee_details.find(name => name.full_name === f_name);
+
+if(!result) {
+    console.log("User doesn't exist.")
+    return;
+}
+console.log("User exists.");
+console.log(`Full Name: ${result.full_name}`);
+console.log(`Employee ID: ${result.employee_id}`);
+console.log(`Email ID: ${result.email}`);
+console.log(`City: ${result.city}`);
+console.log(`Phone Number: ${result.phone_no}`);
+console.log(`State: ${result.state}`);
+}
+
